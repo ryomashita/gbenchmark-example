97dbafe7a9d92803f244633ca15513f1acf00bdb
ryomashita <86085692+ryomashita@users.noreply.github.com>
2024-05-01 11:38:47 +0900

    modified:  src/basic_usage.cpp

--- src/basic_usage.cpp
+++ src/basic_usage.cpp
@@ -16,3 +16,23 @@ static void BM_StringCopy(benchmark::State &state) {
 BENCHMARK(BM_StringCopy);
 
 BENCHMARK_MAIN();
+
+// `BENCHMARK_MAIN` Expands to
+// int main(int argc, char **argv) {
+//   char arg0_default[] = "benchmark";
+//   char *args_default = arg0_default;
+//   if (!argv) {
+//     argc = 1;
+//     argv = &args_default;
+//   }
+//   ::benchmark ::Initialize(&argc, argv);
+//   if (::benchmark ::ReportUnrecognizedArguments(argc, argv))
+//     return 1;
+//   ::benchmark ::RunSpecifiedBenchmarks();
+//   ::benchmark ::Shutdown();
+//   return 0;
+// }
+
+// To register benchmarks, use `RegisterBenchmark(name, func, args...)`
+// alternatively.
+// Unlike the macros, `RegisterBenchmark` can be called anywhere.
